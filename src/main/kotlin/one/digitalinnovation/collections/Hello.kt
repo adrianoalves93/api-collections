6212ba1de8fd03ac31a25c9c8339d3412c87f488
adrianoalves93 <adriano.alvescto@gmail.com>
2022-07-19 19:01:27 -0300

    created IntArray test Class

---
+++ src/main/kotlin/one/digitalinnovation/collections/Hello.kt
@@ -0,0 +1,6 @@
+package one.digitalinnovation.collections
+
+fun main(args: Array<String>) {
+    println("Hello, World")
+}
+
---
+++ src/test/kotlin/one/digitalinnovation/collections/HelloTest.kt
@@ -0,0 +1,8 @@
+package one.digitalinnovation.collections
+
+import org.junit.Test
+import kotlin.test.assertEquals
+
+class HelloTest {
+
+}
